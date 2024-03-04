20b9f5e51ed414746703f8a85b1369ab1703e002
jayeshshinde1221 <atharavsawant495@gmail.com>
2024-03-04 17:18:19 +0530

    update

---
+++ Script.js
@@ -0,0 +1,16 @@
+var value1=21;
+var value2=21;
+
+var value=21+21;
+console.log(value);
+
+
+var value=21*21;
+console.log(value);
+
+
+var value=21-21;
+console.log(value);
+
+var value=21/21;
+console.log(value);
