6abd434647b484e1f57d75d37f548278ed1735d1
jayeshshinde <atharavsawant495@gmail.com>
2024-03-06 14:29:20 +0530

    update

--- Script.js
+++ Script.js
@@ -1,16 +1,26 @@
-var value1=21;
-var value2=21;
+//var value1=21;
+//var value2=21;
 
-var value=21+21;
-console.log(value);
-
-
-var value=21*21;
-console.log(value);
+//var value=21+21;
+//console.log(value);
 
 
-var value=21-21;
-console.log(value);
+//var value=21*21;
+//console.log(value);
 
-var value=21/21;
-console.log(value);
+
+//var value=21-21;
+//console.log(value);
+
+//var value=21/21;
+//console.log(value);
+
+
+
+
+
+//var num1 = parseInt(prompt('Enter the first number '));
+//var num2 = parseInt(prompt('Enter the second number '));
+
+//const sum = num1 + num2;
+//console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
